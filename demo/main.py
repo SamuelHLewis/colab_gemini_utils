3e3d62b7d9e7e5e65d89990685555efcd88c05fe
SamuelHLewis <sam.lewis1@crowncommercial.gov.uk>
2025-09-22 13:52:19 +0100

    Add demo codebase

---
+++ demo/main.py
@@ -0,0 +1,4 @@
+from utils import add_util, print_util
+
+total = add_util(1, 1)
+print_util(total)
---
+++ demo/utils.py
@@ -0,0 +1,7 @@
+def print_util(user_input):
+    print(user_input)
+    return
+
+def add_util(num1, num2):
+    sum = num1 + num2
+    return sum
